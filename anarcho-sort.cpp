f49882dd789dc1529421f0083840bf92f828dbea
Kurt B <Kontakt@karlosbubi.de>
2020-09-26 16:44:14 +0200

    typo

--- anarcho-sort.cpp
+++ anarcho-sort.cpp
@@ -1,5 +1,5 @@
 /*
-Dosen't do anything, the lis may arrange itself however it pleases.
+Dosen't do anything, the elemants may arrange themselfs however they please.
 */
 
 void anarchoSort(int* in){

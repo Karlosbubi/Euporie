5fbcbfbb790bac9c4b3aa0cf5a03d5fa3338a950
kurt <Kontakt@Karlosbubi.de>
2020-10-26 14:03:12 +0100

    extended and fixed hitler sort

--- hitler-sort.cpp
+++ hitler-sort.cpp
@@ -1,14 +1,30 @@
 /*
-Eliminates every "non superior" Element.
+Eliminates every "non superior" Element,
+then Bubble sorts.
 */
 
-void hitlerSort(int* in, int& length, int superior){
+void hitlerSort(int* in, int& length, int* superior){
     int count = 0;
+
     for(int i = 0; i < length; i++){
-        if(in[i] == superior){
-            in[count] = superior;
-            count++;
+        for(auto j : superior){
+            if(in[i] == superior){
+                in[count] = superior;
+                count++;
+            }
         }
     }
+
     length = count;
+
+    int temp = 0;
+    for(int i = 0; i < length; i++){
+        for(int j = 1; j < length; i++){
+            if(in[j] < in[j-1]){
+                temp = in[j-1];
+                in[j-1] = in[j];
+                in[j] = temp;
+            }
+        }
+    }
 }
